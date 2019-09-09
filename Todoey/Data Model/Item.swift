0b0951ca043c89d15db22ad11fd839a06db7d023
Anh <anh.bui737@gmail.com>
2019-09-09 22:43:27 +0800

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bui Anh on 9/9/19.
+//  Copyright © 2019 Bui Anh. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title = ""
+  var done = false
+}
